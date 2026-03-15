e8936d5f9dce40d765867fdd629bac6b82824867
durgacharan22 <dcharanpotukuru@gmail.com>
2026-03-14 20:54:51 -0400

    feat: Add AdSense component for ad integration in layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import GoogleAnalytics from "@/components/GoogleAnalytics";
+import AdSense from "@/components/AdSense";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -32,6 +33,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <AdSense />
         <GoogleAnalytics />
         {children}
       </body>
